43d011376f4e122ace88935d1481fba1d7f9154d
aritrakoley <koley.aritra824@gmail.com>
2023-02-24 19:44:24 +0530

    Code cleanup #1

--- front-end/src/staff-app/providers/RollProvider.tsx
+++ front-end/src/staff-app/providers/RollProvider.tsx
@@ -14,13 +14,6 @@ const initialState: StateType = {
     name: "",
     rollState: "",
   },
-  rollOptions: {
-    rollMap: new Map(),
-  },
-}
-
-type RollOptionsType = {
-  rollMap: Map<number, RolllStateType>
 }
 
 type StateType = {
@@ -28,47 +21,12 @@ type StateType = {
   isRollMode: boolean
   sortOptions: SortOptionsType
   filterOptions: FilterOptionsType
-  rollOptions: RollOptionsType
 }
 
 type ContextType = {
   state: StateType
   dispatch: Dispatch<{ type: string; payload: any }>
 }
-
-// Helpers -----------------
-// const sortStudents = (list: any[], sortOptions: SortOptionsType) => {
-//   const { asc, byFirstName } = sortOptions
-
-//   const studentCompare = (a: any, b: any) => {
-//     const _a = byFirstName ? a.first_name : a.last_name
-//     const _b = byFirstName ? b.first_name : b.last_name
-
-//     const res = _a.localeCompare(_b)
-//     return asc ? res : -res
-//   }
-
-//   list.sort(studentCompare)
-//   return [...list]
-// }
-
-// const filterStudents = (list: any[], filterOptions: FilterOptionsType) => {
-//   const { name, rollState } = filterOptions
-//   return list.filter(
-//     (e) => `${e.first_name} ${e.last_name}`.toLowerCase().includes(name.toLowerCase()) && (!rollState || rollState.toString() === "all" || e.rollState === rollState)
-//   )
-// }
-
-// const markAttendance = (list: any[], rollOptions: RollOptionsType) => {
-//   const { rollMap } = rollOptions
-//   if (!isRollMode || !rollMap.size) return list
-//   return list.map((e) => ({ ...e, rollState: rollMap.get(e.id) }))
-// }
-
-// const updateList = (list: any[], sortOptions: SortOptionsType, filterOptions: FilterOptionsType, rollOptions: RollOptionsType) => {
-//   return filterStudents(markAttendance(sortStudents(list, sortOptions), rollOptions), filterOptions)
-// }
-//--------------------------
 
 export const RollContext = createContext<ContextType>({ state: initialState, dispatch: () => {} })
 
@@ -105,7 +63,6 @@ const reducer = (state: any, action: { type: string; payload: any }) => {
 
     case "mark":
       console.log("mark")
-      // state.rollOptions.rollMap.set(payload.id, payload.rollState)
       const newList = state.studentList.map((s: Person) => (s.id === payload.id ? { ...s, rollState: payload.rollState } : s))
       return {
         ...state,
